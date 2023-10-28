da9b497b91093764ec9090498a6b2b1fafb0c095
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2023-10-28 23:18:29 +0530

    Revert "project managed by gql"

--- src/skill-member/dto/update-skill-member.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateSkillMemberInput } from './create-skill-member.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateSkillMemberInput extends PartialType(CreateSkillMemberInput) {
-  @Field(() => Int)
-  id: number;
-}
--- src/skill-project/dto/update-skill-project.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateSkillProjectInput } from './create-skill-project.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateSkillProjectInput extends PartialType(CreateSkillProjectInput) {
-  @Field(() => Int)
-  id: number;
-}
